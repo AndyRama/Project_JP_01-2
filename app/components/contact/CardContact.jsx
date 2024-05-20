7667e0e37039a83b65be3d1de8dcb28a3440c241
Andy Ramaroson <andyramaroson@gmail.com>
2024-05-20 23:58:02 +0200

    (feat)[Contact] add component CardContact and cardInfo

---
+++ app/components/contact/CardContact.jsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from 'react'
+import CardInfo from 'app/components/contact/CardInfo'
+
+
+const CardContact = ({ className }) => (
+  <section className={` ${className}`}>
+    <div className="container mx-auto mt-[-30] lg:mt-[-50px]">
+      <div className="flex justify-around">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-auto">
+          <CardInfo className=" w-12/12 lg:w-5/12 px-0 lg:px-4 xl:px-16 mb-10 md:pr-4" />
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default CardContact
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SubHero from '../components/about/SubHero'
 import ContactForm from '../components/contact/ContactForm'
+import CardInfo from '../components/contact/CardInfo'
 
 const page = () => {
   return (
     <>
       <SubHero className="pt-32 pb-24" title="Contact" subTitle=""/>
+      <CardInfo/>
       <ContactForm />
     </>
   )
