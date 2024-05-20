6ebeeff78365d49da790571542d6debbd23aa132
Andy Ramaroson <andyramaroson@gmail.com>
2024-05-20 18:20:44 +0200

    (feat)[Home] add card Description Right

---
+++ app/components/CardDescriptionR.jsx
@@ -0,0 +1,164 @@
+'use client'
+
+import Image from 'next/image'
+import React, { useRef } from 'react'
+import { motion, useScroll, useTransform } from 'framer-motion'
+
+const cardDescriptionRContent = {
+  heading: {
+    title: 'Philosophie',
+    subTitle: 'Mes valeurs',
+  },
+  content1: {
+    img1: '/images/colaborate.jpg',
+    description:
+      'Allier entraînement rigoureux et nutrition équilibrée pour un mode de vie sain et durable. Je crois fermement que la réussite en matière de fitness réside dans un équilibre entre un entraînement bien structuré et une alimentation adaptée.',
+  },
+}
+
+const CardDescriptionR = ({ className }) => {
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start end', 'end start'],
+  })
+
+  const img1 = useTransform(scrollYProgress, [0, 1], ['20%', '-20%'])
+
+  return (
+    <section className={`${className}]`} ref={ref}>
+      <div className="container px-4 mx-auto ">
+        {/* Main Card Description - content - Left */}
+        <div className="md:flex justify-center rounded-md py-0 ">
+          <div className="lg:w-8/12 lg:flex gap-20 items-center">
+            <div className="mb-7 lg:mb-0 md:w-6/12 lg:order-2 relative ">
+              {/* Card Description - subTitle - Left */}
+              {cardDescriptionRContent.heading.subTitle && (
+                <motion.span
+                  initial={{ opacity: 0, y: 20 }}
+                  whileInView={{
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                      delay: 0.2,
+                      duration: 0.5,
+                    },
+                  }}
+                  viewport={{ once: true }}
+                  className="uppercase tracking-[2px] text-[12.5px] mb-2 inline-block text-orange-400 content sm:hidden"
+                >
+                  {cardDescriptionRContent.heading.subTitle}
+                </motion.span>
+              )}
+
+              {/* CardDescriptionContent - Left - Title */}
+              {cardDescriptionRContent.heading.title && (
+                <motion.h2
+                  initial={{ opacity: 0, y: 20 }}
+                  whileInView={{
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                      delay: 0.3,
+                      duration: 0.5,
+                    },
+                  }}
+                  viewport={{ once: true }}
+                  className="text-2xl lg:text-4xl text-white mb-7 content sm:hidden"
+                >
+                  {cardDescriptionRContent.heading.title}
+                </motion.h2>
+              )}
+
+              {/* Card Description - Image - Left */}
+              <motion.div style={{ y: img1 }} className="z-[2] relative">
+                <Image
+                  src="/images/colaborate.jpg"
+                  className="object-cover
+                  !w-[600] !h-[600] lg:h-[400px] lg:w-[400px] object-center rounded-md"
+                  width={400}
+                  height={600}
+                  alt="coaching personel Image"
+                />
+              </motion.div>
+
+              {/* Card Description - description Left - 1 */}
+              <motion.p
+                initial={{ opacity: 0, y: 10 }}
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    delay: 0.4,
+                    duration: 0.5,
+                  },
+                }}
+                viewport={{ once: true }}
+                className="leading-relaxed mb-10 text-white text-justify content sm:hidden "
+              >
+                {cardDescriptionRContent.content1.description}
+              </motion.p>
+            </div>
+
+            <div className="lg:w-6/12">
+              {/* Card Description - subTitle - Left */}
+              {cardDescriptionRContent.heading.subTitle && (
+                <motion.span
+                  initial={{ opacity: 0, y: 20 }}
+                  whileInView={{
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                      delay: 0.2,
+                      duration: 0.5,
+                    },
+                  }}
+                  viewport={{ once: true }}
+                  className="uppercase tracking-[2px] text-[12.5px] mb-2  text-orange-400 md:visible invisible "
+                >
+                  {cardDescriptionRContent.heading.subTitle}
+                </motion.span>
+              )}
+
+              {/* CardDescriptionContent - Left - Title */}
+              {cardDescriptionRContent.heading.title && (
+                <motion.h2
+                  initial={{ opacity: 0, y: 20 }}
+                  whileInView={{
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                      delay: 0.3,
+                      duration: 0.5,
+                    },
+                  }}
+                  viewport={{ once: true }}
+                  className="text-2xl lg:text-4xl text-white mb-7 md:visible invisible "
+                >
+                  {cardDescriptionRContent.heading.title}
+                </motion.h2>
+              )}
+              {/* Card Description - description Left - 1 */}
+              <motion.p
+                initial={{ opacity: 0, y: 10 }}
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    delay: 0.4,
+                    duration: 0.5,
+                  },
+                }}
+                viewport={{ once: true }}
+                className="leading-relaxed mb-10 text-white text-justify md:visible invisible "
+              >
+                {cardDescriptionRContent.content1.description}
+              </motion.p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default CardDescriptionR
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero"
 import CardDescriptionL from "./components/CardDescriptionL"
+import CardDescriptionR from "./components/CardDescriptionR"
 import RecentBlog from "./components/RecentBlog"
 import Subscribe from "./components/Subscribe"
 
@@ -8,6 +9,7 @@ export default function Home() {
     <>
       <Hero className="pt-32 md:pt-22 pb-0 lg-pb:10" />
       <CardDescriptionL className=" pt-14 pb-15" />
+      <CardDescriptionR className=" pt-14 pb-15" />
       <RecentBlog className="pt-14 pb-15 lg:pb-32"/>
       <Subscribe className="py-16 pt-64 -mt-48 lg:py-32"/>
     </>
