40ea45c7aef96f7f1c7770fe9840e323965310f3
Andy Ramaroson <andyramaroson@gmail.com>
2024-05-20 18:25:59 +0200

    (feat)[Home] add card Grid Image

---
+++ app/components/CardGridImage.jsx
@@ -0,0 +1,83 @@
+'use client'
+import React from 'react'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+
+const cards = [
+  {
+    title: '#Arcachon #Team #Vélo #Event',
+    years: '#2023',
+    image: '/images/groupArcachon.jpg',
+  },
+  {
+    title: '#Compétition #FR #Fitness',
+    years: '#2024',
+    image: '/images/fitnessFrance.jpg',
+  },
+  {
+    title: '#Training #Remise #en #forme',
+    years: '#2023',
+    image: '/images/reducation.jpg',
+  },
+  {
+    title: '#Concours #Prépa #Jeremy Prat',
+    years: '#2024',
+    image: '/images/coaching.jpg',
+  },
+]
+
+const CardImage = ({ card, index }) => {
+  const delay = index * 0.05
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          delay,
+          duration: 0.5,
+        },
+      }}
+      viewport={{ once: true }}
+      className="bg-transparent relative overflow-hidden group rounded"
+    >
+      <Image
+        src={card.image}
+        alt={card.title}
+        width={400}
+        height={400}
+        className="object-cover object-center h-[300px] duration-300
+            transition-all ease-in-out group-hover:scale-[1.1]"
+      />
+      <div>
+        <p className=" uppercase text-[13px] mt-5 text-left tracking-[1px] text-orange-400 font-bold">
+          {card.title} {card.years}
+        </p>
+      </div>
+    </motion.div>
+  )
+}
+
+const CardGridImage = ({ className }) => {
+  return (
+    <section className={`${className}`}>
+      <div className="container px-4 mx-auto rounded-md ">
+        <div className="py-16 mt-[-20rem] md:mt-0 md:flex justify-center mb-2 ">
+          <div
+            className="grid grid-cols-1 md:grid-cols-2
+             lg:grid-cols-2 xl:grid-cols-4 gap-6 w-full lg:w-10/12 
+             mx-auto mt-10 text-gray-500 "
+          >
+            {cards.slice(0, 4).map((card, index) => (
+              <CardImage key={index} index={index} card={card} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default CardGridImage
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero"
 import CardDescriptionL from "./components/CardDescriptionL"
 import CardDescriptionR from "./components/CardDescriptionR"
+import CardGridImage from "./components/CardGridImage"
 import RecentBlog from "./components/RecentBlog"
 import Subscribe from "./components/Subscribe"
 
@@ -9,7 +10,8 @@ export default function Home() {
     <>
       <Hero className="pt-32 md:pt-22 pb-0 lg-pb:10" />
       <CardDescriptionL className=" pt-14 pb-15" />
-      <CardDescriptionR className=" pt-14 pb-15" />
+      <CardDescriptionR className=" relativee pt-14 pb-15 sm:bg-[#1D1D1D]" />
+      <CardGridImage />
       <RecentBlog className="pt-14 pb-15 lg:pb-32"/>
       <Subscribe className="py-16 pt-64 -mt-48 lg:py-32"/>
     </>
