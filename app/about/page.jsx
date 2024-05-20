7c9bf5dd6a29608f840c9e42135284b5a6ed880a
Andy Ramaroson <andyramaroson@gmail.com>
2024-05-20 16:38:18 +0200

    (fix) mistake error import

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
-import Hero from '../components/about/SubHero'
-import Story from '../components/about/Story'
+import SubHero from '../components/about/SubHero'
 import Subscribe from '../components/Subscribe'
-// import Testimonial from '../components/Testimonial'
-import Testimonial from '../components/Testimonial2'
-import WhyUs from '../components/about/WhyUs'
-import Team from '../components/about/Team'
 
 
 const aboutPage = () => {
   return (
     <>
-      <Hero className="pt-52 pb-24" />
-      <Story/>
-      <WhyUs className="pt-52 pb-0"/>
-      <Testimonial className="pt-10 lg:pt-44 pb-10 lg:pb-32"/>
-      <Team className="py-10 lg:py-32"/>
+      <SubHero className="pt-52 pb-24" />
       <Subscribe className="py-5 lg:py-32 bg-orange-300"/>
     </>
   )
