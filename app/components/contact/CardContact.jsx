92a056bcdb956a879d5a879d1eec30e598f9508c
Andy Ramaroson <andyramaroson@gmail.com>
2024-05-21 12:40:40 +0200

    (feat)[Contact] add componentCardPricing with link

--- app/components/contact/CardContact.jsx
+++ app/components/contact/CardContact.jsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import CardInfo from 'app/components/contact/CardInfo'
 import CardMap from 'app/components/contact/CardMap'
+import CardPricing from 'app/components/contact/CardPricing'
 
 const CardContact = ({ className }) => (
   <section className={`${className}`}>
@@ -11,6 +12,7 @@ const CardContact = ({ className }) => (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-auto">
           <CardInfo className="w-12/12 lg:w-5/12 px-0 lg:px-4 xl:px-16 mb-10 md:pr-4" />
           <CardMap className="w-12/12 lg:w-5/12 px-0 lg:px-4 xl:px-16 mb-10" />
+          <CardPricing className="w-12/12 lg:w-5/12 px-0 lg:px-4 xl:px-16 mb-10" />
         </div>
       </div>
     </div>
