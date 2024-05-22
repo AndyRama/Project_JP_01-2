6e1397e8b5a697941cab55bc0697c211e7a1925f
Andy Ramaroson <andyramaroson@gmail.com>
2024-05-22 19:43:22 +0200

    (feat)[Team] create component hero

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SubHero from '../components/about/SubHero'
 import Subscribe from '../components/Subscribe'
 import Story from '../components/about/Story'
+import Hero from '../components/about/Hero'
 
 const Page = () => {
   return (
@@ -12,6 +13,7 @@ const Page = () => {
         subTitle="nos valeurs"
       />
       <Story />
+      <Hero />
       <Subscribe className="py-16 pt-32 lg:py-32" />
     </>
   )
